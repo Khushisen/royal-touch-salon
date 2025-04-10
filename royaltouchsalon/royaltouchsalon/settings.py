fd0c0b6a81a2567346fc68ac564288da9762b154
khushisen <khushisen9001@gmail.com>
2025-04-10 15:25:46 +0530

    secured email

--- royaltouchsalon/royaltouchsalon/settings.py
+++ royaltouchsalon/royaltouchsalon/settings.py
@@ -123,10 +123,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'khushisen9001@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DFAULT_FROM_EMAIL = EMAIL_HOST_USER
-ADMIN_EMAIL = 'khushisen9001@gmail.com'
+ADMIN_EMAIL = config('EMAIL_HOST_USER')
 
 RAZOR_KEY_ID = config("RAZOR_KEY_ID")
 RAZOR_KEY_SECRET = config("RAZOR_KEY_SECRET")
